7521656a2538d67bc14b6d8105ee798a69364cc4
Eccenux <eccenux+git@gmail.com>
2020-07-12 03:04:15 +0200

    0.2: open red links on pending changes

--- pendingChangesHelper.user.js
+++ pendingChangesHelper.user.js
@@ -1,8 +1,8 @@
 ﻿// ==UserScript==
 // @name         Wiki Pending Changes Helper
 // @namespace    pl.enux.wiki
-// @version      0.1.1
-// @description  [0.1.1] Pomocnik do przeglądania strona na Wikipedii.
+// @version      0.2.0
+// @description  [0.2.0] Pomocnik do przeglądania strona na Wikipedii.
 // @author       Nux; Beau; Matma Rex
 // @match        https://pl.wikipedia.org/*
 // @grant        none
