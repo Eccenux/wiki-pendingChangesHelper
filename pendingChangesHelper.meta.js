cfb2f158c3966be6a13fcb02d106578b6eee900d
Nux <eccenux+git@gmail.com>
2022-09-15 19:04:54 +0200

    4.7

--- pendingChangesHelper.meta.js
+++ pendingChangesHelper.meta.js
@@ -1,8 +1,8 @@
 ﻿// ==UserScript==
 // @name         Wiki Pending Changes Helper
 // @namespace    pl.enux.wiki
-// @version      4.6.0
-// @description  [4.6.0] Pomocnik do przeglądania strona na Wikipedii.
+// @version      4.7.0
+// @description  [4.7.0] Pomocnik do przeglądania strona na Wikipedii.
 // @author       Nux; Beau; Matma Rex
 // @match        https://pl.wikipedia.org/*
 // @grant        none
