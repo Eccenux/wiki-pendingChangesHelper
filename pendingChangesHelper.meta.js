ff98075e315686397d5c22028481d02e6406ff88
eccenux <eccenux+git@gmail.com>
2023-07-05 00:18:36 +0200

    link update

--- pendingChangesHelper.meta.js
+++ pendingChangesHelper.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Wiki Pending Changes Helper
 // @namespace    pl.enux.wiki
-// @version      5.1.2
-// @description  Pomocnik do przeglądania strona na Wikipedii. Na pl.wiki: [[Wikipedia:Narzędzia/Pending Changes Helper]], [[MediaWiki:Gadget-oldreviewedpages.js]].
+// @version      5.2.0
+// @description  Pomocnik do przeglądania strona na Wikipedii. Na pl.wiki: [[Wikipedia:Narzędzia/Pending Changes Helper]], [[MediaWiki:Gadget-pendingChangesHelper.js]].
 // @author       Nux; Beau; Matma Rex
 // @match        https://pl.wikipedia.org/*
 // @grant        none
