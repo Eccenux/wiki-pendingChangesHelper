1b7e43e5fbeb34c610b0ce30d1564611cbe0d100
eccenux <eccenux+git@gmail.com>
2023-07-05 01:22:13 +0200

    command bar +deploy fix

--- wikiploy-dev.js
+++ wikiploy-dev.js
@@ -2,12 +2,12 @@
  * Dev/staging deploy.
  */
 import { DeployConfig, Wikiploy } from 'wikiploy';
-import { build_js, readVersion } from './build';
+import { build_js, readVersion } from './build.js';
 
 const ployBot = new Wikiploy();
 
 (async () => {
-	let version = await readVersion();
+	let version = await readVersion('package.json');
 	// custom summary
 	ployBot.summary = () => {
 		return `v${version}: JS builder`;
