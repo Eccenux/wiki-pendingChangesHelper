66afd9f79b9ca03a075095ec9aec9bbbce17f8ed
eccenux <eccenux+git@gmail.com>
2023-07-05 02:58:08 +0200

    Wikideploy release (as a gadget)

---
+++ wikiploy.js
@@ -0,0 +1,27 @@
+/**
+ * Deploy as a gadget.
+ */
+import { DeployConfig, Wikiploy } from 'wikiploy';
+import { build_js, readVersion } from './build.js';
+
+const ployBot = new Wikiploy();
+
+(async () => {
+	let version = await readVersion('package.json');
+	// custom summary
+	ployBot.summary = () => {
+		return `v${version}: JS builder`;
+	}
+
+	await build_js();
+
+	const configs = [];
+	configs.push(new DeployConfig({
+		src: 'pendingChangesHelper.mw.js',
+		dst: 'MediaWiki:Gadget-pendingChangesHelper.js',
+	}));
+	await ployBot.deploy(configs);
+})().catch(err => {
+	console.error(err);
+	process.exit(1);
+});
